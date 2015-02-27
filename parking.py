914302d18ad819bae80caddc1754e68249355f48
Dugucloud <dugucloud@qq.com>
2015-02-28 00:14:21 +0800

    判断质数时减小因数搜索范围

--- parking.py
+++ parking.py
@@ -10,19 +10,19 @@ import random
 def parking(low, high):
     if high - low < 1:
         return 0
-        
+
     else:
         x = random.uniform(low,high - 1) #车头停靠位置
         return parking(low, x) + 1 + parking(x + 1, high)
-        
+
 s = 0
 for i in range(1000):
     s += parking(0, 5)
-    
+
 s = s / 1000.0
-    
-    
+
+
 print s
-       
-  
-        
+
+
+
--- primenumber.py
+++ primenumber.py
@@ -4,18 +4,19 @@ Created on Sat Jan 03 10:21:20 2015
 
 @author: tianyi
 """
+
+from math import sqrt
+
 while True:
-    
-    
     x = int(raw_input("input x:"))
-    
-    for i in range(2,x):
+
+    for i in range(2, int(sqrt(x) + .01) + 1):
         if x % i == 0:
             print "not prime"
             break
     else:
         print "prime number"
-        
+
     ch = raw_input('quit?:')
     if ch ==  "q":
         break
